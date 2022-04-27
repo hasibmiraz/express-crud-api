69f13458632548a2180bf61c5173b7b5651c8cee
hasibmiraz <hasibmiraz09@gmail.com>
2022-04-28 01:10:36 +0600

    Fetch all data

--- routerHandler/todoHandler.js
+++ routerHandler/todoHandler.js
@@ -6,7 +6,25 @@ import todoSchema from '../schemas/todoSchema.js';
 const Todo = new mongoose.model('Todo', todoSchema);
 
 // Get all todos
-router.get('/', async (req, res) => {});
+router.get('/', async (req, res) => {
+  await Todo.find({ status: 'active' })
+    .select({
+      _id: 0,
+      __v: 0,
+      data: 0,
+    })
+    .limit(2)
+    .exec((err, data) => {
+      err
+        ? res.status(500).json({
+            error: 'There was a server side error',
+          })
+        : res.status(200).json({
+            result: data,
+            message: 'Success',
+          });
+    });
+});
 
 // Get single todo
 router.get('/:id', async (req, res) => {});
