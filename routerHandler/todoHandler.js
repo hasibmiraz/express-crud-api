b51db756d51227d2c14d0b9f7492b2966141864f
hasibmiraz <hasibmiraz09@gmail.com>
2022-04-28 00:15:51 +0600

    Mongoose many todo insert applied

--- routerHandler/todoHandler.js
+++ routerHandler/todoHandler.js
@@ -15,20 +15,28 @@ router.get('/:id', async (req, res) => {});
 router.post('/', async (req, res) => {
   const newTodo = new Todo(req.body);
   await newTodo.save((err) => {
-    if (err) {
-      res.status(500).json({
-        error: 'There was a server side error',
-      });
-    } else {
-      res.status(200).json({
-        message: 'Todo was inserted successfully',
-      });
-    }
+    err
+      ? res.status(500).json({
+          error: 'There was a server side error',
+        })
+      : res.status(200).json({
+          message: 'Todo was inserted successfully',
+        });
   });
 });
 
 // Post multiple todo
-router.post('/all', async (req, res) => {});
+router.post('/all', async (req, res) => {
+  await Todo.insertMany(req.body, (err) => {
+    err
+      ? res.status(500).json({
+          error: 'There was a server side error',
+        })
+      : res.status(200).json({
+          message: 'Todos were inserted successfully',
+        });
+  });
+});
 
 // put todo
 router.put('/:id', async (req, res) => {});
